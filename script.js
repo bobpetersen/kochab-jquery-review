8f95fa7f7e2a0e044309fa2d5a778bed01a43193
bobpetersen <33petersen@gmail.com>
2018-04-12 13:16:46 -0500

    functional event handlers

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('jQuery is Ready');
+// our very first callback function
+$(document).ready(onReady);
+
+console.log('jQuery intro Lecture');
+
+function onReady() {
+    console.log('jQuery is ready');
+    $('h1').addClass('funny');
+    $('h1').css('color', 'red');
+
+    //getters and setters
+    console.log($('h2').text()); // get text value of h2
+    $('h2').text('You have been set!!!');
+
+
+    // inputs (also Getter and Setter)
+    $('#nameInput').val('Bob'); //setter for input val
+    console.log($('#nameInput').val());
+
+    // events
+    // event handler
+    $('#clickableButton').on('click', function() {
+        //console.log($('#nameInput').val());
+        console.log($('nameInput').val());
+    });
+    
+    $('#secondButton').on('click', function() {
+            console.log('I was clicked');
+        });
+}
